bfd38026589c394eac26798e078b61029f585582
Mathieu Delmenico <madelmen@c2r8s2.42lausanne.ch>
2024-11-01 18:22:37 +0100

    broken project

---
+++ srcb/main.c
@@ -0,0 +1,11 @@
+#include "../include/ft_printf_bonus.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%5%");
+    printf("\n\n");
+    ft_printf("%5%");
+    printf("\n\n");
+    return (0);
+}
